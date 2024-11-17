87e2bacc431fb8d40ce9f383bcc00b983181106f
shenchris <hqshen2-c@my.cityu.edu.hk>
2024-11-17 02:34:17 -0500

    View table

--- webserver/server.py
+++ webserver/server.py
@@ -1,219 +1,106 @@
-#!/usr/bin/env python3
-
-"""
-Columbia W4111 Intro to databases
-Example webserver
-
-To run locally
-
-    python server.py
-
-Go to http://localhost:8111 in your browser
-
-
-A debugger such as "pdb" may be helpful for debugging.
-Read about it online.
-"""
-
 import os
+
 from sqlalchemy import *
 from sqlalchemy.pool import NullPool
+from flask import Flask
 from flask import Flask, request, render_template, g, redirect, Response
+import traceback
 
 tmpl_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates')
 app = Flask(__name__, template_folder=tmpl_dir)
-
-
-
-# XXX: The Database URI should be in the format of: 
-#
-#     postgresql://USER:PASSWORD@<IP_OF_POSTGRE_SQL_SERVER>/<DB_NAME>
-#
-# For example, if you had username ewu2493, password foobar, then the following line would be:
-#
-#     DATABASEURI = "postgresql://ewu2493:foobar@<IP_OF_POSTGRE_SQL_SERVER>/postgres"
-#
-# For your convenience, we already set it to the class database
+names = []
 
 # Use the DB credentials you received by e-mail
-DB_USER = "YOUR_DB_USERNAME_HERE"
-DB_PASSWORD = "YOUR_DB_PASSWORD_HERE"
-
+DB_USER = "cch2201"
+DB_PASSWORD = "cch2201"
 DB_SERVER = "w4111.cisxo09blonu.us-east-1.rds.amazonaws.com"
-
-DATABASEURI = "postgresql://"+DB_USER+":"+DB_PASSWORD+"@"+DB_SERVER+"/proj1part2"
-
-
-#
-# This line creates a database engine that knows how to connect to the URI above
-#
-engine = create_engine(DATABASEURI)
-
-
-# Here we create a test table and insert some values in it
-engine.execute("""DROP TABLE IF EXISTS test;""")
-engine.execute("""CREATE TABLE IF NOT EXISTS test (
-  id serial,
-  name text
-);""")
-engine.execute("""INSERT INTO test(name) VALUES ('grace hopper'), ('alan turing'), ('ada lovelace');""")
-
-
+DATABASEURI = "postgresql://"+DB_USER+":"+DB_PASSWORD+"@"+DB_SERVER+"/w4111"
 
 @app.before_request
 def before_request():
-  """
-  This function is run at the beginning of every web request 
-  (every time you enter an address in the web browser).
-  We use it to setup a database connection that can be used throughout the request
+    engine = create_engine(DATABASEURI)
+    conn = engine.connect()
+    conn.execute(text("""SET search_path TO cch2201;"""))
+    # Here we create a test table and insert some values in it
 
-  The variable g is globally accessible
-  """
-  try:
-    g.conn = engine.connect()
-  except:
-    print("uh oh, problem connecting to database")
-    import traceback; traceback.print_exc()
-    g.conn = None
+    try:
+        g.conn = engine.connect()
+    except:
+        print("uh oh, problem connecting to database")
+        traceback.print_exc()
+        g.conn = None
 
 @app.teardown_request
 def teardown_request(exception):
-  """
-  At the end of the web request, this makes sure to close the database connection.
-  If you don't the database could run out of memory!
-  """
   try:
     g.conn.close()
   except Exception as e:
     pass
 
-
-#
-# @app.route is a decorator around index() that means:
-#   run index() whenever the user tries to access the "/" path using a GET request
-#
-# If you wanted the user to go to e.g., localhost:8111/foobar/ with POST or GET then you could use
-#
-#       @app.route("/foobar/", methods=["POST", "GET"])
-#
-# PROTIP: (the trailing / in the path is important)
-# 
-# see for routing: http://flask.pocoo.org/docs/0.10/quickstart/#routing
-# see for decorators: http://simeonfranklin.com/blog/2012/jul/1/python-decorators-in-12-steps/
-#
 @app.route('/')
-def index():
-  """
-  request is a special object that Flask provides to access web request information:
+def books():
+    # Retrieve search criteria from the form
+    search_query = request.args.get('search', '')
+    search_by = request.args.get('search_by', 'Name')  # Search by ID, Name, or Author
+    sort_by = request.args.get('sort_by', 'BID')  # Sort by Popularity, Quantity, or Name
+    sort_order = request.args.get('order', 'asc')  # Sort order (asc or desc)
 
-  request.method:   "GET" or "POST"
-  request.form:     if the browser submitted a form, this contains the data in the form
-  request.args:     dictionary of URL arguments e.g., {a:1, b:2} for http://localhost?a=1&b=2
+    # Define the base SQL query
+    query = "SELECT * FROM book"
 
-  See its API: http://flask.pocoo.org/docs/0.10/api/#incoming-request-data
-  """
+    # Apply search criteria if a search query is provided
+    if search_query:
+        query += f" WHERE {search_by} LIKE '%{search_query}%'"
 
-  # DEBUG: this is debugging code to see what request looks like
-  print(request.args)
+    # Apply sorting based on selected criteria
+    if sort_by == 'Quantity':
+        query += f" ORDER BY Quantity {sort_order}"
+    elif sort_by == 'Name':
+        query += f" ORDER BY Name {sort_order}"
+    # elif sort_by == 'Popularity':
+    #     query += f" ORDER BY Popularity {sort_order}"  # Assuming Popularity column exists
+    else:
+        query += f" ORDER BY BID {sort_order}"
 
-
-  #
-  # example of a database query
-  #
-  cursor = g.conn.execute("SELECT name FROM test")
-  names = []
-  for result in cursor:
-    names.append(result['name'])  # can also be accessed using result[0]
-  cursor.close()
-
-  #
-  # Flask uses Jinja templates, which is an extension to HTML where you can
-  # pass data to a template and dynamically generate HTML based on the data
-  # (you can think of it as simple PHP)
-  # documentation: https://realpython.com/blog/python/primer-on-jinja-templating/
-  #
-  # You can see an example template in templates/index.html
-  #
-  # context are the variables that are passed to the template.
-  # for example, "data" key in the context variable defined below will be 
-  # accessible as a variable in index.html:
-  #
-  #     # will print: [u'grace hopper', u'alan turing', u'ada lovelace']
-  #     <div>{{data}}</div>
-  #     
-  #     # creates a <div> tag for each element in data
-  #     # will print: 
-  #     #
-  #     #   <div>grace hopper</div>
-  #     #   <div>alan turing</div>
-  #     #   <div>ada lovelace</div>
-  #     #
-  #     {% for n in data %}
-  #     <div>{{n}}</div>
-  #     {% endfor %}
-  #
-  context = dict(data = names)
+    # Execute the query with parameters if search query exists
+    print(query)
+    cursor = g.conn.execute(text(query))
+    data = []
+    for i in cursor:
+        data.append(list(i))
+    print(data)
+    # Pass the book data to the template
+    return render_template('index.html', data=data)
 
 
-  #
-  # render_template looks in the templates/ folder for files.
-  # for example, the below file reads template/index.html
-  #
-  return render_template("index.html", **context)
-
-#
-# This is an example of a different path.  You can see it at
-# 
-#     localhost:8111/another
-#
-# notice that the functio name is another() rather than index()
-# the functions for each app.route needs to have different names
-#
 @app.route('/another')
 def another():
-  return render_template("anotherfile.html")
+    # Retrieve search criteria from the form
+    search_query = request.args.get('search', '')
+    search_by = request.args.get('search_by', 'Users')  # Search by ID, Name, or Author
 
 
-# Example of adding new data to the database
-@app.route('/add', methods=['POST'])
-def add():
-  name = request.form['name']
-  print(name)
-  cmd = 'INSERT INTO test(name) VALUES (:name1), (:name2)';
-  g.conn.execute(text(cmd), name1 = name, name2 = name);
-  return redirect('/')
+    # Define the base SQL query
+    query = "SELECT * FROM "
 
+    if search_by:
+        query += search_by
 
-@app.route('/login')
-def login():
-    abort(401)
-    this_is_never_executed()
+    # Execute the query with parameters if search query exists
+    print(query)
+    cursor = g.conn.execute(text(query))
+    data = []
+    for i in cursor:
+        data.append(list(i))
+
+    keys = []
+    cursor = g.conn.execute(text(query)).keys()
+    for i in cursor:
+        keys.append(i)
+    # Pass the book data to the template
+    return render_template('another.html', name=search_by, keys=keys, data=data)
 
 
 if __name__ == "__main__":
-  import click
-
-  @click.command()
-  @click.option('--debug', is_flag=True)
-  @click.option('--threaded', is_flag=True)
-  @click.argument('HOST', default='0.0.0.0')
-  @click.argument('PORT', default=8111, type=int)
-  def run(debug, threaded, host, port):
-    """
-    This function handles command line parameters.
-    Run the server using
-
-        python server.py
-
-    Show the help text using
-
-        python server.py --help
-
-    """
-
-    HOST, PORT = host, port
-    print("running on %s:%d" % (HOST, PORT))
-    app.run(host=HOST, port=PORT, debug=debug, threaded=threaded)
-
-
-  run()
+    print(tmpl_dir)
+    app.run()
