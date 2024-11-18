1429a51e511531c767a3890b78af7b9891ed5311
shenchris <hqshen2-c@my.cityu.edu.hk>
2024-11-17 20:50:52 -0500

    network setting

--- webserver/server.py
+++ webserver/server.py
@@ -4,6 +4,7 @@ from sqlalchemy import *
 from sqlalchemy.pool import NullPool
 from flask import Flask, request, render_template, g, redirect, Response, jsonify
 import traceback
+import click
 
 tmpl_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates')
 app = Flask(__name__, template_folder=tmpl_dir)
@@ -321,5 +322,15 @@ def another():
 
 
 if __name__ == "__main__":
-    print(tmpl_dir)
-    app.run()
+    @click.command()
+    @click.option('--debug', is_flag=True)
+    @click.option('--threaded', is_flag=True)
+    @click.argument('HOST', default='0.0.0.0')
+    @click.argument('PORT', default=8111, type=int)
+    def run(debug, threaded, host, port):
+
+
+        HOST, PORT = host, port
+        print("running on %s:%d" % (HOST, PORT))
+        app.run(host=HOST, port=PORT, debug=debug, threaded=threaded)
+    run()
