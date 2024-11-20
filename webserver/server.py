9e0a98317955694aecb657b92a580af5c76a2bc5
shenchris <hqshen2-c@my.cityu.edu.hk>
2024-11-19 19:51:55 -0500

    security update

--- webserver/server.py
+++ webserver/server.py
@@ -22,7 +22,6 @@ def before_request():
     engine = create_engine(DATABASEURI)
     conn = engine.connect()
     conn.execute(text("""SET search_path TO cch2201;"""))
-    # Here we create a test table and insert some values in it
 
     try:
         g.conn = engine.connect()
@@ -42,71 +41,70 @@ def teardown_request(exception):
 def books():
     data = []
     keys = []
-    parameters={}
+    parameters = {}
     query = "select Book.BID, quantity -  (select count(*) from Loan_Record where return is null and Book.bid = Loan_Record.bid) Copy, Book.name Name, author, Category.name Category, Publisher.name Publisher from Book, Book_Category, Category, Book_Publisher, Publisher where Book.bid = Book_Category.bid and Book_Category.cid= Category.cid and Book_Publisher.bid = book.bid and Book_Publisher.pid = Publisher.pid "
-    # query = "select Book.BID bid, quantity -  (select count(*) from Loan_Record where return is null and Book.bid = Loan_Record.bid) Copy, Book.name Name, author from Book"
-    # query = "select * from book"
+
 
     if request.method == 'POST':
-        search_by = request.form['search_by']
-        print(search_by)
-        sort_by = request.form['sort_by']
-        sort_order = request.form['order']
         parameters = {
             'search_query': "%"+request.form['search']+"%",
+            'search_by': request.form['search_by'],
+            'sort_by': request.form['sort_by'],
+            'sort_order': request.form['order']
         }
+        sort_by_option = ["Copy", "Name", "BID"]
+        search_by_option = ["Book.name", "Author", "Category.name", "Publisher.name"]
+        query += " and "
+        if parameters['search_by'] in search_by_option:
+            query += parameters['search_by']
+        query += " LIKE  :search_query ORDER BY "
 
-        print(parameters['search_query'])
-        query += " and " + search_by + " LIKE  :search_query ORDER BY " + sort_by + " " + sort_order
+        if parameters['sort_by'] in sort_by_option:
+            query += parameters['sort_by']
+
+        if parameters['sort_order'] == "asc":
+            query += " asc"
+        else:
+            query += " desc"
         print(text(query), parameters)
-        cursor = g.conn.execute(text(query), parameters)
-    else:
-        cursor = g.conn.execute(text(query))
+
+    cursor = g.conn.execute(text(query), parameters)
 
     for i in cursor:
         data.append(list(i))
 
-    cursor = g.conn.execute(text(query),parameters).keys()
+    cursor = g.conn.execute(text(query), parameters).keys()
     for i in cursor:
         keys.append(i)
 
     return render_template('index.html', keys=keys, data=data)
 @app.route('/borrow', methods=['POST'])
 def borrow_book():
-
     try:
         # Get data from the request
         request_data = request.get_json()
-        user_id = str(request_data['UID'])
-        book_id = str(request_data['BID'])
-        issue = datetime.date.today().strftime("%Y-%m-%d")
-        due = (datetime.date.today()+ datetime.timedelta(days=7)).strftime("%Y-%m-%d")
         parameters = {
-            'book_id': book_id,
-            'issue': issue,
-            'due': due,
-            'user_id': user_id,
-
+            'book_id': request_data['BID'],
+            'issue': datetime.date.today().strftime("%Y-%m-%d"),
+            'due': (datetime.date.today() + datetime.timedelta(days=7)).strftime("%Y-%m-%d"),
+            'user_id': request_data['UID']
         }
 
-        check = "select quantity -  (select count(*) from Loan_Record where return is null and Book.bid = Loan_Record.bid) count from Book where bid = " + book_id
-        check_result = g.conn.execute(text(check)).fetchone()
+        check = "select quantity -  (select count(*) from Loan_Record where return is null and Book.bid = Loan_Record.bid) count from Book where bid = :book_id"
+        print(text(check), parameters)
+        check_result = g.conn.execute(text(check), parameters).fetchone()
         if int(check_result[0]) <= 0:
-            return jsonify({'success': False})
-        # check_query = "select Loan_Record.loanid, uid, bid from User_Loan_Record, Loan_Record where User_Loan_Record.LoanID = Loan_Record.LoanID and Loan_Record.bid = :book_id and uid = :user_id"
-        # check_result = g.conn.execute(text(check_query), parameters).fetchone()
-        # print(check_result)
+            return jsonify({'Fail': False})
+
 
         insert_query = """
         INSERT INTO Loan_Record (BID, Issue, Due, Return)
         VALUES (:book_id, :issue, :due, NULL)
         RETURNING LoanID
         """
-        # print(text(insert_query), parameters)
         result = g.conn.execute(text(insert_query), parameters)
         g.conn.commit()
         loan_id = str(result.fetchone()[0])
-        # print("loan_id:"+loan_id)
         parameters['loan_id'] = loan_id
 
         insert_user_loan_query = """
@@ -119,31 +117,22 @@ def borrow_book():
 
     except Exception as e:
         print(f"Error: {e}")
-        return jsonify({'success': False})
+        return jsonify({'Fail': False})
 @app.route('/return', methods=['POST'])
 def return_book():
-    request_data = request.get_json()
-    loan_id = request_data['loan_id']
-    # check_query = "SELECT * FROM Loan_Record WHERE loanid = :loan_id"
-    # check_result = g.conn.execute(text(check_query), {'loan_id': loan_id}).fetchone()
-    # print(check_result)
     try:
-        current_date = datetime.date.today().strftime("%Y-%m-%d")
+        request_data = request.get_json()
         query = """
             UPDATE Loan_Record
             SET Return = :return_date
             WHERE LoanID = :loan_id
         """
         params = {
-            'return_date': current_date,
-            'loan_id': loan_id  # Make sure loan_id is passed as a parameter
+            'return_date': datetime.date.today().strftime("%Y-%m-%d"),
+            'loan_id': request_data['loan_id']  # Make sure loan_id is passed as a parameter
         }
         g.conn.execute(text(query), params)
         g.conn.commit()
-
-        # check_query = "SELECT * FROM Loan_Record WHERE loanid = :loan_id"
-        # check_result = g.conn.execute(text(check_query), {'loan_id': loan_id}).fetchone()
-        # print(check_result)
         return jsonify({'success': True})
 
     except Exception as e:
@@ -152,63 +141,63 @@ def return_book():
 @app.route('/user_loan', methods=['get','post'])
 def user_loan():
     data = []
-    keys=[]
-    # uid = request.args.get('UID')
+    keys = []
+    parameters = {}
+    try:
+        query = "select users.uid, users.name, Loan_Record.loanid, Loan_Record.bid, Loan_Record.issue, Loan_Record.due, Loan_Record.return  from users, User_Loan_Record, Loan_Record where users.uid = User_Loan_Record.uid and User_Loan_Record.LoanID = Loan_Record.LoanID and return is NULL"
+        if request.method == 'POST':
+            parameters['uid'] = request.form['UID']
+            if parameters['uid']:
+                query += " and users.uid = :uid"
 
-    query = "select users.uid, users.name, Loan_Record.loanid, Loan_Record.bid, Loan_Record.issue, Loan_Record.due, Loan_Record.return  from users, User_Loan_Record, Loan_Record where users.uid = User_Loan_Record.uid and User_Loan_Record.LoanID = Loan_Record.LoanID and return is NULL"
+        cursor = g.conn.execute(text(query), parameters)
+        for i in cursor:
+            data.append(list(i))
+        cursor = g.conn.execute(text(query), parameters).keys()
+        for i in cursor:
+            keys.append(i)
+    except Exception as e:
+        print(f"Error: {e}")
+        return jsonify({'Fail': False})
 
-    if request.method == 'POST':
-        uid = request.form['UID']
-        if uid:
-            query += f" and users.uid = {uid}"
-    cursor = g.conn.execute(text(query))
-    for i in cursor:
-        data.append(list(i))
-    cursor = g.conn.execute(text(query)).keys()
-    for i in cursor:
-        keys.append(i)
     return render_template('user_loan.html', keys=keys, data=data)
 @app.route('/study_room', methods=['get','post'])
 def study_room():
     data = []
     keys = []
+    parameters ={}
     # uid = request.args.get('UID')
 
-    query = "select lid, room_number,Study_Room.RoomID, uid, date,start_time,end_time from Study_Room, Book_Study_Room where Study_Room.RoomID = Book_Study_Room.RoomID"
+    query = "select Booking_ID, lid, room_number,Study_Room.RoomID, uid, date,start_time,end_time from Study_Room, Book_Study_Room where Study_Room.RoomID = Book_Study_Room.RoomID"
 
     if request.method == 'POST':
-        uid = request.form['UID']
-        if uid:
-            query += f" and users.uid = {uid}"
-    cursor = g.conn.execute(text(query))
+        parameters['UID'] = request.form['UID']
+        if parameters['UID']:
+            query += " and users.uid = :uid"
+    cursor = g.conn.execute(text(query), parameters)
     for i in cursor:
         data.append(list(i))
-    cursor = g.conn.execute(text(query)).keys()
+    cursor = g.conn.execute(text(query), parameters).keys()
     for i in cursor:
         keys.append(i)
     return render_template('study_room.html', keys=keys, data=data)
 @app.route('/book_study', methods=['POST'])
 def book_study():
     if request.method == 'POST':
-        uid = request.form['UID']
-        room_id = request.form['room_id']
         start = request.form['from']
         time_obj = datetime.datetime.strptime(start, "%H:%M")
         new_time_obj = time_obj + datetime.timedelta(hours=1)
-        end = new_time_obj.strftime("%H:%M")
-        date = request.form['date']
         parameters ={
-            'uid':uid,
-            'room_id':room_id,
+            'uid': request.form['UID'],
+            'room_id': request.form['room_id'],
             'start': start,
-            'end':end,
-            'date': date
+            'end': new_time_obj.strftime("%H:%M"),
+            'date': request.form['date']
 
         }
-        print(uid,room_id,start,end, date)
 
-        check = "select count(*) from Book_Study_Room where RoomID = "+ room_id + " and Start_time = :start and Date =:date"
-        check_result = g.conn.execute(text(check),parameters).fetchone()
+        check = "select count(*) from Book_Study_Room where RoomID = :room_id and Start_time = :start and Date =:date"
+        check_result = g.conn.execute(text(check), parameters).fetchone()
         print(int(check_result[0]))
         if int(check_result[0]) > 0:
             return jsonify({'Already booked': False})
@@ -226,7 +215,7 @@ def book_study():
             return f"""
             <h1>Booking Successful!</h1>
             <p>Your Booking_ID is: <strong>{Booking_ID}</strong></p>
-            <p>Your Booking_ID is from: <strong>{start}</strong> to <strong>{end}</strong> on <strong>{date}</strong> </p>
+            <p>Your Booking_ID is from: <strong>{start}</strong> to <strong>{parameters['end']}</strong> on <strong>{parameters['date']}</strong> </p>
             <a href="/study_room">Click here to return to the booking page</a>
             """
         except Exception as e:
@@ -237,80 +226,78 @@ def book_study():
     
 @app.route('/cancel_study', methods=['POST'])
 def cancel_study():
-    # Parse the incoming JSON data
+    request_data = request.get_json()
+    parameters = {
+        'booking_id': request_data['booking_id']
+    }
+
     try:
-        request_data = request.get_json()
-        booking_id = request_data['booking_id']
-
-        # Check if the booking exists
-        check_query = "SELECT COUNT(*) FROM Book_Study_Room WHERE Booking_ID = :booking_id"
-        check_result = g.conn.execute(text(check_query), {'booking_id': booking_id}).fetchone()
-
-        if check_result[0] == 0:
-            # Booking does not exist
-            return jsonify({'success': False, 'message': 'Booking ID does not exist'})
-
-        # Perform the delete query
-        delete_query = "DELETE FROM Book_Study_Room WHERE Booking_ID = :booking_id"
-        g.conn.execute(text(delete_query), {'booking_id': booking_id})
+        query = "DELETE FROM Book_Study_Room WHERE Booking_ID = :booking_id"
+        # query = f"DELETE FROM Book_Study_Room WHERE Booking_ID = {booking_id}"
+        g.conn.execute(text(query), parameters)
         g.conn.commit()
-
         return jsonify({'success': True})
 
     except Exception as e:
         print(f"Error: {e}")
-        return jsonify({'success': False, 'message': 'An error occurred while canceling the booking'})
-
+        return jsonify({'Fail': False})
 @app.route('/order', methods=['GET', 'POST'])
 def order():
     data = []
     keys = []
-    parameters={}
+    parameters = {}
     query = "select sid, orders.oid, bid, rid, lid,price, date,quantity from orders, manage where orders.oid = manage.oid"
 
-    cursor = g.conn.execute(text(query), parameters)
-    for i in cursor:
-        data.append(list(i))
+    try:
+        cursor = g.conn.execute(text(query), parameters)
+        for i in cursor:
+            data.append(list(i))
 
-    cursor = g.conn.execute(text(query),parameters).keys()
-    for i in cursor:
-        keys.append(i)
-
+        cursor = g.conn.execute(text(query),parameters).keys()
+        for i in cursor:
+            keys.append(i)
+    except Exception as e:
+        print(f"Error: {e}")
+        return jsonify({'Fail': False})
     return render_template('order.html', keys=keys, data=data)
 
 @app.route('/make_order', methods=['POST'])
 def make_order():
     data = []
     keys = []
-    if request.method == 'POST':
-        parameters ={
-            'sid':request.form['sid'],
-            'bid': request.form['bid'],
-            'rid': request.form['rid'],
-            'lid': request.form['lid'],
-            'price': request.form['price'],
-            'quantity': request.form['quantity'],
-            'date': request.form['date']
+    try:
+        if request.method == 'POST':
+            parameters ={
+                'sid': request.form['sid'],
+                'bid': request.form['bid'],
+                'rid': request.form['rid'],
+                'lid': request.form['lid'],
+                'price': request.form['price'],
+                'quantity': request.form['quantity'],
+                'date': request.form['date']
 
-        }
+            }
 
-        insert_query = """
-        INSERT INTO orders (BID, RID, LID, Price, Date, Quantity)
-        VALUES (:bid, :rid, :lid, :price, :date, :quantity)
-        RETURNING OID
-        """
-        result = g.conn.execute(text(insert_query), parameters)
-        g.conn.commit()
-        oid = str(result.fetchone()[0])
-        print("oid:"+oid)
-        parameters['oid'] = oid
+            insert_query = """
+            INSERT INTO orders (BID, RID, LID, Price, Date, Quantity)
+            VALUES (:bid, :rid, :lid, :price, :date, :quantity)
+            RETURNING OID
+            """
+            result = g.conn.execute(text(insert_query), parameters)
+            g.conn.commit()
+            oid = str(result.fetchone()[0])
+            print("oid:"+oid)
+            parameters['oid'] = oid
 
-        insert_user_loan_query = """
-        INSERT INTO manage (SID, OID)
-        VALUES (:sid, :oid)
-        """
-        g.conn.execute(text(insert_user_loan_query), parameters)
-        g.conn.commit()
+            insert_user_loan_query = """
+            INSERT INTO manage (SID, OID)
+            VALUES (:sid, :oid)
+            """
+            g.conn.execute(text(insert_user_loan_query), parameters)
+            g.conn.commit()
+    except Exception as e:
+        print(f"Error: {e}")
+        return jsonify({'Fail': False})
 
     return jsonify({f'success': True})
 
@@ -318,31 +305,36 @@ def make_order():
 def another():
     data = []
     keys = []
-
-    if request.method == 'POST':
-        # Get data from a form submission
-        search_by = request.form['search_by']
-    else:
-        # Get data from query parameters
-        search_by = "Users"
-
+    parameters = {}
     query = "SELECT * FROM "
-    query += search_by
+    try:
+        if request.method == 'POST':
+            # Get data from a form submission
+            parameters['search_by'] = request.form['search_by']
+        else:
+            # Get data from query parameters
+            parameters['search_by'] = "Users"
+        search_by_option = ["Users", "Library", "Access", "Staff", "Staff_belong_Library", "Study_Room", "Book_Study_Room", "Book", "Store", "Loan_Record", "user_loan_record", "publisher", "Book_Publisher", "Retailer", "orders", "manage", "Category", "Book_Category", "subcategory"]
+        if parameters['search_by'] in search_by_option:
+            query += parameters['search_by']
+        else:
+            raise ValueError(f"Invalid table name: {parameters['search_by']}")
 
-    print(query)
 
-    # run query for data
-    cursor = g.conn.execute(text(query))
-    for i in cursor:
-        data.append(list(i))
+        # run query for data
+        cursor = g.conn.execute(text(query),parameters)
+        for i in cursor:
+            data.append(list(i))
 
-    # run query for keys
-    cursor = g.conn.execute(text(query)).keys()
-    for i in cursor:
-        keys.append(i)
-
+        # run query for keys
+        cursor = g.conn.execute(text(query),parameters).keys()
+        for i in cursor:
+            keys.append(i)
+    except Exception as e:
+        print(f"Error: {e}")
+        return jsonify({'Fail': False})
     # Pass the data to the template
-    return render_template('another.html', name=search_by, keys=keys, data=data)
+    return render_template('another.html', name=parameters['search_by'], keys=keys, data=data)
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
